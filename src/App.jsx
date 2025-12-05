9c47fa80c1d383813e44f439737423fca1fcdc16
Esly Fabrizio Díaz Pleitez <fabriziodiaz21@gmail.com>
2025-12-04 21:39:23 -0600

    Fix react-router-dom imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route} from 'react-router';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { NotFound } from './pages/NotFound';
-import { Layout } from './pages/Layout';
-import { PokemonDetails } from './pages/PokemonDetails';
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { NotFound } from './pages/NotFound'
+import { Layout } from './pages/Layout'
+import { PokemonDetails } from './pages/PokemonDetails'
 
 function App() {
   return (
     <Routes>
-      <Route element={<Layout/>}>
+      <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/detalles/:id" element={<PokemonDetails/>} />
+        <Route path="/detalles/:id" element={<PokemonDetails />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Route>
