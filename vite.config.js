4b5a33ac44f0586cce1795c78ffa181d6dc2572e
Esly Fabrizio Díaz Pleitez <fabriziodiaz21@gmail.com>
2025-12-04 22:12:37 -0600

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/SPA202503-MAIN/',  // 👈 ESTO ES LO IMPORTANTE
+  plugins: [react(), tailwindcss()],
+  base: '/SPA202503-MAIN/',
 })
